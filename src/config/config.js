0501951917dfaaf18d0066d4e01ca27fb11dbb19
Abdulmoniem <abdulmoniem.alazzaly@gmail.com>
2025-08-01 02:43:02 +0300

    init

---
+++ src/config/config.js
@@ -0,0 +1,38 @@
+const config = {
+  data: {
+    title: "Wedding of Abdulmoniem & Doha", // This will be generated automatically
+    description:
+      "We are getting married and happily invite you to celebrate this special moment with us.", // This can be randomized later
+    groomName: "Abdulmoniem",
+    brideName: "Doha",
+    parentGroom: "Son of Mr. Groom & Mrs. Groom",
+    parentBride: "Daughter of Mr. Bride & Mrs. Bride",
+    date: "2025-08-06",
+    maps_url: "https://maps.app.goo.gl/TWtNfx9qaJsnt4t78",
+    maps_embed:
+      "https://www.google.com/maps/place/%D9%82%D8%A7%D8%B9%D8%A9+%D8%A7%D9%84%D9%85%D8%A7%D8%B3%D8%A9+%D8%AF%D9%85%D9%86%D9%87%D9%88%D8%B1%E2%80%AD/@31.0188988,30.463095,17z/data=!3m1!4b1!4m6!3m5!1s0x14f66b4c0637af3d:0x8ca8a98a0bc1da72!8m2!3d31.0188942!4d30.4656699!16s%2Fg%2F11t49jbdb5?entry=ttu&g_ep=EgoyMDI1MDcyOS4wIKXMDSoASAFQAw%3D%3D",
+    time: "9:00 - 12:00 ",
+    location: "Rose Almasah, Damanhour",
+    address: "Rose Almasa, Damanhour, Beheira Governorate",
+    ogImage: "/images/og-image.jpg",
+    favicon: "/images/favicon.ico",
+    agenda: [
+      {
+        title: "Rose Almasah, Damanhour",
+        date: "2025-08-06",
+        startTime: "21:00",
+        endTime: "24:00",
+        location: "",
+        address: "Rose Almasah, Damanhour",
+      }
+    ],
+    audio: {
+      src: "/audio/Mahmoud-El-Esseily.mp3", // or /audio/nature-sound.mp3
+      title: "Mahmoud-El-Esseily-El-Lella.mp3", // or Nature Sound
+      autoplay: true,
+      loop: true
+    }
+  }
+};
+
+export default config;
---
+++ src/pages/MainContent.jsx
@@ -0,0 +1,14 @@
+import Hero from '@/pages/Hero'
+import Events from '@/pages/Events'
+
+
+// Main Invitation Content
+export default function MainContent() {
+    return (
+        <>
+            <Hero />
+            <Events />
+            
+        </>
+    )
+}
