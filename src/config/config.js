a7e9f980df1759ae0902866128278a1543c1f038
Abdulmoniem <abdulmoniem.alazzaly@gmail.com>
2025-08-05 11:21:17 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -11,7 +11,7 @@ const config = {
     maps_url: "https://maps.app.goo.gl/TWtNfx9qaJsnt4t78",
     maps_embed:
       "https://www.google.com/maps/place/%D9%82%D8%A7%D8%B9%D8%A9+%D8%A7%D9%84%D9%85%D8%A7%D8%B3%D8%A9+%D8%AF%D9%85%D9%86%D9%87%D9%88%D8%B1%E2%80%AD/@31.0188988,30.463095,17z/data=!3m1!4b1!4m6!3m5!1s0x14f66b4c0637af3d:0x8ca8a98a0bc1da72!8m2!3d31.0188942!4d30.4656699!16s%2Fg%2F11t49jbdb5?entry=ttu&g_ep=EgoyMDI1MDcyOS4wIKXMDSoASAFQAw%3D%3D",
-    time: "9:00 - 12:00 ",
+    time: "21:00 - 24:00 ",
     location: "Rose Almasah, Damanhour",
     address: "Rose Almasa, Damanhour, Beheira Governorate",
     ogImage: "/images/og-image.jpg",
